8a0353d6a5b99ee94e9e969f452cc19d38c294b9
LAPTOP-TUT28EOU\ASUS <tanadon.chaij@bumail.net>
2021-01-29 20:06:02 +0700

    Send Assignment Week1

---
+++ Network/Assets/Find.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Find : MonoBehaviour
+{
+    public string[] Data5;
+    public Text Pim;
+    public Text Show;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Finddata()
+    {
+        for (int t = 0; t <= 4; t++)
+        {
+            if (Pim.text == Data5[t])
+            {
+                Show.text = "[" + "<color=green>"+ Pim.text +"</color>" + "] is found.";
+                return;
+            }
+            else
+            {
+                Show.text = "[" + "<color=red>" + Pim.text + "</color>" + "] is not found"; 
+                return;
+            }
+
+        }
+    }
+}
